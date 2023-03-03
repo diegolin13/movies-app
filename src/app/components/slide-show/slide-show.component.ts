e71b000f9dc9fe3a23e81495946132c5f00423ec
DJR <djr@gmail.com>
2023-03-03 12:25:43 -0600

    Se implementa un carrusel de imagenes de peliculas

--- src/app/components/slide-show/slide-show.component.ts
+++ src/app/components/slide-show/slide-show.component.ts
@@ -1,17 +1,38 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { Movies } from 'src/app/interfaces/cartelera-response';
+import Swiper, { Navigation, Pagination, Autoplay } from 'swiper';
+Swiper.use([Navigation, Autoplay]);
 
 @Component({
   selector: 'app-slide-show',
   templateUrl: './slide-show.component.html',
   styleUrls: ['./slide-show.component.css']
 })
-export class SlideShowComponent implements OnInit {
+export class SlideShowComponent implements OnInit, AfterViewInit {
   @Input() movies: Movies[] = [];
+  mySwiper?: Swiper; 
+
+  ngAfterViewInit() {
+    this.mySwiper = new Swiper('.swiper', {
+      loop: true,
+      speed: 400,
+      spaceBetween: 100,
+      effect: 'fade',
+      autoplay: {
+        delay: 10000,
+      },
+    });
+  }
 
   ngOnInit() {
-    console.log('en el side show')
-    console.log(this.movies);
+  }
+
+  onSlideNext() {
+    this.mySwiper!.slideNext();
+  }
+
+  onSlidePrev() {
+    this.mySwiper!.slidePrev();
   }
 
 }
